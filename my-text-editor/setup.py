bf6b03965e6279908270bfebdd7ec5672864e0ec
Simon Palma <simon@MacBook-Pro-de-Simon.local>
2024-11-18 11:07:45 +0100

    adding setup.py and requirements.txt

---
+++ my-text-editor/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.1",
+    packages=find_packages(include=[]),
+    install_requires=[],
+    author="Simon Palma",
+    author_email="simonpc55@gmail.com",
+    description="",
+    url="",
+)
